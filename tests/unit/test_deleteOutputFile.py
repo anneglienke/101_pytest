13051e96f6066e32241c74d6b36e12737724d65e
Anne Glienke <anneglienke@Paulos-MacBook-Pro.local>
2023-06-14 16:17:38 +0100

    feat: add test_deleteOutputFiles_fail

---
+++ tests/unit/test_deleteOutputFile.py
@@ -0,0 +1,30 @@
+from src.main import deleteOutputFiles
+import os
+import pytest
+
+
+def test_deleteOutputFiles(tmp_path, caplog):
+    # Create temporary files in the output directory
+    output_dir = tmp_path / "output"
+    output_dir.mkdir()
+    file1 = output_dir / "file1.txt"
+    file1.touch()
+    file2 = output_dir / "file2.txt"
+    file2.touch()
+
+    # Call the function under test
+    deleteOutputFiles(output_dir)
+
+    # Check if files are deleted
+    assert not file1.exists()
+    assert not file2.exists()
+
+
+@pytest.mark.xfail(reason="Test fails due to missing output directory")
+def test_deleteOutputFiles_xfail():
+    output_path = "./output"
+
+    deleteOutputFiles(output_path)
+
+    # Assertions
+    assert not os.path.exists(output_path)
