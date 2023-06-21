c6e797ed8599d21a6e8c2be51bd8dbcb646506a8
Anne Glienke <anneglienke@Paulos-MacBook-Pro.local>
2023-06-21 17:24:19 +0100

    refactor: make it easier to test

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ OUTPUT_CONFIG = {
     "products": ["product_id", "product_name"],
     "order_items": ["order_id", "customer_id", "product_id", "quantity", "price_gbp"],
 }
-ORDER_SCHEMA = {
+ORDERS_SCHEMA = {
     "order_id": {
         "type": "integer",
         "required": True,
@@ -73,37 +73,40 @@ ORDER_SCHEMA = {
     },
 }
 
+
 # Set up logs
 logging.basicConfig(format="[%(asctime)s] %(message)s", level=logging.INFO)
 
 
-# Use a Cerberus to validate dicts according to ORDER_SCHEMA
-def validateObjects(order):
-    v = Validator(ORDER_SCHEMA)
+# Use a Cerberus to validate dicts according to schema
+def validator(order, schema):
+    v = Validator(schema)
     return v.validate(order), v.errors
+
+
+def validateObjects(objects, schema):
+    valid_objects = []
+    for obj in objects:
+        valid, errors = validator(obj, schema)
+        if valid:
+            valid_objects.append(obj)
+        else:
+            logging.info(f'Discarded invalid object(s): "{obj}", errors: {errors}')
+    valid_rows = len(valid_objects)
+    return valid_objects, valid_rows
 
 
 # Open a .jsonl file and transform into a list of Python dicts
 def extractData(dataset: str):
     # Open JSON file
     input_json = open(dataset, "r")
-    list_obj = []
+    extracted_objects = []
     # Iterate through each JSON object, convert JSON string into Python dict
     for obj in input_json:
         py_dict = json.loads(obj)
-        list_obj.append(py_dict)
-    logging.info(f"Loaded {len(list_obj)} rows.")
-
-    # Validate each obj in the list according to ORDER_SCHEMA
-    valid_obj = []
-    for obj in list_obj:
-        valid, errors = validateObjects(obj)
-        if valid:
-            valid_obj.append(obj)
-        else:
-            logging.info(f'Discarded invalid object(s): "{obj}", errors: {errors}')
-    valid_rows = len(valid_obj)
-    return valid_obj, valid_rows
+        extracted_objects.append(py_dict)
+    logging.info(f"Loaded {len(extracted_objects)} rows.")
+    return extracted_objects
 
 
 # Transform order list by initializing a DataFrame with flattened nested values and renamed columns
@@ -122,7 +125,7 @@ def transformOrdersList(valid_orders: list, valid_rows: int):
     return renamed_df
 
 
-# Deletes files on ./output/ directory
+# Delete existing files on ./output/ directory
 def deleteOutputFiles(output_path: str):
     # Get a list of all the files in the directory
     files = os.listdir(output_path)
@@ -135,12 +138,23 @@ def deleteOutputFiles(output_path: str):
     logging.info("Deleted all the files in the output directory")
 
 
-# Assemble output data as configured in OUTPUT_CONFIG, remove duplicates and create CSV files
-def loadData(df: pd.DataFrame):
-    # Delete existing files in the ./output/ directory
-    deleteOutputFiles(OUTPUT_PATH)
+def createOutputDir(dir):
+    # Get the current working directory
+    current_dir = os.getcwd()
 
-    # Create new files in the ./output/ directory
+    # Create the path for the "tests" folder
+    tests_dir = os.path.join(current_dir, dir)
+
+    # Create the path for the "output" folder inside the "tests" folder
+    output_dir = os.path.join(tests_dir, "output")
+
+    # Create the "output" folder if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+
+# Assemble output data as configured in OUTPUT_CONFIG, remove duplicates and create CSV files
+def loadData(df: pd.DataFrame, output_path):
     for file_name, cols in OUTPUT_CONFIG.items():
         # Drop duplicates
         deduplicated_df = df.drop_duplicates(
@@ -148,19 +162,23 @@ def loadData(df: pd.DataFrame):
         )
         # Create CSV file
         deduplicated_df.to_csv(
-            f"{OUTPUT_PATH}{file_name}.csv",
+            f"{output_path}{file_name}.csv",
             sep=",",
             columns=cols,
             header=True,
             index_label="index",
         )
+        print("loaded to csv")
         logging.info(f'Created CSV file "{file_name}.csv"')
 
 
 def execute():
-    valid_orders, valid_rows = extractData(INPUT_PATH)
+    extracted_objects = extractData(INPUT_PATH)
+    valid_orders, valid_rows = validateObjects(extracted_objects, ORDERS_SCHEMA)
     df = transformOrdersList(valid_orders, valid_rows)
-    loadData(df)
+    createOutputDir("src")
+    deleteOutputFiles(OUTPUT_PATH)
+    loadData(df, OUTPUT_PATH)
 
 
 execute()
